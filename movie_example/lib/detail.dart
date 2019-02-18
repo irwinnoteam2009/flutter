c8a39824fc54eb17fe88cf0576b7cad18c477c37
HellBoys <irwin.noteam.2009@gmail.com>
2019-02-18 18:39:59 +0300

    add movie info

--- movie_example/lib/detail.dart
+++ movie_example/lib/detail.dart
@@ -45,6 +45,23 @@ class MovieDetail extends StatelessWidget {
   final Movie _movie;
   MovieDetail(this._movie);
 
+  Widget itemInfo(String key, value) => Row(
+        children: <Widget>[
+          Text(
+            key,
+            style: TextStyle(
+              color: Colors.blueAccent,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          Text(
+            value,
+            textAlign: TextAlign.end,
+          )
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,6 +90,7 @@ class MovieDetail extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
                 child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     GestureDetector(
                       onTap: () {
@@ -82,14 +100,24 @@ class MovieDetail extends StatelessWidget {
                       },
                       child: Hero(
                         tag: 'movie_${_movie.id}',
-                        child: Container(
-                          height: 200,
-                          child: Hero(
-                            tag: 'poster',
-                            child: Image.network(_movie.posterPath,
-                                fit: BoxFit.cover),
-                          ),
+                        child: Hero(
+                          tag: 'poster',
+                          child: Image.network(_movie.posterPath,
+                              fit: BoxFit.cover, 
+                              height: 200,),
                         ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          itemInfo('Release:', '${_movie.releaseAt.year}-${_movie.releaseAt.month}-${_movie.releaseAt.day}'),
+                          itemInfo('Popularity:', _movie.popularity.toString()),
+                          itemInfo('Vote:', '${_movie.voteAvg} (${_movie.voteCount})'),
+                          itemInfo('Genre:', _movie.genres.map((it) => it.name).toList().join(",")),
+                        ],
                       ),
                     )
                   ],
